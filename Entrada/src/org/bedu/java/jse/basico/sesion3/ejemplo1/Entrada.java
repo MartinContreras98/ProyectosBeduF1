adcbfaedf08275179e3d99a960e613196c1394fc
MartinContreras98 <martinja1998@gmail.com>
2023-01-19 20:14:47 -0600

    Creacion de Reto1/Sesion3

--- Entrada/src/org/bedu/java/jse/basico/sesion3/ejemplo1/Entrada.java
+++ Entrada/src/org/bedu/java/jse/basico/sesion3/ejemplo1/Entrada.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 
 public class Entrada {
     public static void main(String[] args){
-     /*   Scanner scanner = new Scanner(System.in);
+     /*   Scanner scanner = new Scanner(System.in);//dese new con un constructor
         System.out.println("Escribe tu nombre: ");
         String nombre = scanner.nextLine();
         System.out.println("Hola " + nombre);*/
-        Console console = System.console();
+        Console console = System.console();//Generando desde un metodo de otra clase
         if (console == null){
             System.err.println("No hay consola.");
-            System.exit(1);
+            System.exit(1);// 0- termino bien, 1- termino con error
         }
-        String nombre = console.readLine("%s", "Escribe tu nombre: ");
+        String nombre = console.readLine("%s", "Escribe tu nombre: ");//Especificador de formato s- String
         System.out.println("Hola " + nombre);
     }
 }
---
+++ Reto1Sesion3/src/org/bedu/java/jse/basico/sesion3/reto1/Vocales.java
@@ -0,0 +1,21 @@
+package org.bedu.java.jse.basico.sesion3.reto1;
+
+import java.awt.print.Printable;
+import java.util.Scanner;
+
+public class Vocales {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escribe una oracion: ");
+        String palabra = scanner.nextLine();
+        int vocales = 0;
+
+        for (char caracter : palabra.toLowerCase().toCharArray()){
+            if (caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u'){
+                vocales ++;
+            }
+        }
+        System.out.println("El numero de vocales es: "+ vocales);
+
+    }
+}
