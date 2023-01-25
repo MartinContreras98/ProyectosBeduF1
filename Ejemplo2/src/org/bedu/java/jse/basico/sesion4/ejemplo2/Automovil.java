683e03b9b6461013d99f90e087db7b9fb45ae74e
MartinContreras98 <martinja1998@gmail.com>
2023-01-24 21:56:13 -0600

    Creacion ejemplo2/sesion4

---
+++ Ejemplo2/src/org/bedu/java/jse/basico/sesion4/ejemplo2/Automovil.java
@@ -0,0 +1,19 @@
+package org.bedu.java.jse.basico.sesion4.ejemplo2;
+
+import java.sql.SQLOutput;
+
+public class Automovil {
+    private String marca, modelo;
+    private int anioFabricacion, kilometraje;
+
+    public Automovil(String marca, String modelo, int anioFabricacion, int kilometraje){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anioFabricacion = anioFabricacion;
+        this.kilometraje = kilometraje;
+    }
+    
+
+
+
+}
---
+++ Ejemplo2/src/org/bedu/java/jse/basico/sesion4/ejemplo2/Ejemplo2.java
@@ -0,0 +1,7 @@
+package org.bedu.java.jse.basico.sesion4.ejemplo2;
+
+public class Ejemplo2 {
+    public static void main(String[] args) {
+
+    }
+}
